0f286304b90434367f2353b888347862a44994e5
dts1234 <adas.kazmierczak@gmail.com>
2021-03-23 10:30:21 +0100

    inital commit

---
+++ src/main/java/uep/diet/manager/dietmanager/DietManagerApplication.java
@@ -0,0 +1,13 @@
+package uep.diet.manager.dietmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietManagerApplication.class, args);
+	}
+
+}
